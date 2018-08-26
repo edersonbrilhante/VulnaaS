fae6012c328d2bf58ab86d2f9423c12a089c6332
Rafael dos Santos <rafaveira3@gmail.com>
2018-08-25 22:14:51 -0300

    [FEAT] Add ReceiveRequest function to api routes
    ReceiveRequest returns a script to do further enumeration of the box
    being used to install the vulnerability.

--- api/routes.go
+++ api/routes.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo"
 )
@@ -9,4 +10,34 @@ import (
 // HealthCheck is the heath check function.
 func HealthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "WORKING!\n")
+}
+
+// ReceiveRequest returns a script to do further enumeration of the box being used
+// to install the vulnerability.
+func ReceiveRequest(c echo.Context) error {
+
+	var scriptCheck string
+
+	// check if vulnaasID is an integer.
+	vulnaasID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return err
+	}
+
+	// vulnaasID > 1000 are Unix based.
+	if vulnaasID > 1000 {
+		scriptCheck = "" +
+			"#!/bin/bash\n" +
+			"#\n" +
+			"which yum 1> /dev/null\n" +
+			"if [ $? -eq 0 ]; then\n" +
+			"echo \"Found yum\"\n" + // curl to vulnaas-api based on yum need to be done
+			"else\n" +
+			"echo \"Not found\"\n" + // curl to vulnaas-api based on apt-get need to be done
+			"fi\n"
+	} else {
+		//scriptCheck = "PS1 script would go here"
+	}
+
+	return c.String(http.StatusOK, scriptCheck)
 }
