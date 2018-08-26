90c062011b24e3ff410e094f0d82357317bf3e2e
Rafael dos Santos <rafaveira3@gmail.com>
2018-08-26 14:32:43 -0300

    [FEAT] Add ReceiveInstallRequest and GetScript

--- api/routes.go
+++ api/routes.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"errors"
+	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 
+	"github.com/Vulnaas/Vulnaas-API/scripts"
 	"github.com/labstack/echo"
 )
 
@@ -12,32 +16,81 @@ func HealthCheck(c echo.Context) error {
 	return c.String(http.StatusOK, "WORKING!\n")
 }
 
-// ReceiveRequest returns a script to do further enumeration of the box being used
+// ReceiveInstallRequest returns a script to do further enumeration of the box being used
 // to install the vulnerability.
-func ReceiveRequest(c echo.Context) error {
+func ReceiveInstallRequest(c echo.Context) error {
 
-	var scriptCheck string
-
-	// check if vulnaasID is an integer.
-	vulnaasID, err := strconv.Atoi(c.Param("id"))
+	// check if ID is an integer.
+	ID, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return err
+		// StatusOK so vagrant up do not crash and send this echo message
+		return c.String(http.StatusOK, "echo \"[x][VulnaaS] Error: ID is not an integer\"")
 	}
 
-	// vulnaasID > 1000 are Unix based.
-	if vulnaasID > 1000 {
-		scriptCheck = "" +
+	var (
+		UnixScript = "" +
 			"#!/bin/bash\n" +
 			"#\n" +
 			"which yum 1> /dev/null\n" +
 			"if [ $? -eq 0 ]; then\n" +
-			"echo \"Found yum\"\n" + // curl to vulnaas-api based on yum need to be done
+			// curl to API to get yum based scripts.
+			fmt.Sprintf("	curl -s http://%s:%s/scripts/yum/%d | sh\n", os.Getenv("API_HOST"), os.Getenv("API_PORT"), ID) +
 			"else\n" +
-			"echo \"Not found\"\n" + // curl to vulnaas-api based on apt-get need to be done
+			// curl to API to get apt-get based scripts.
+			fmt.Sprintf("	curl -s http://%s:%s/scripts/apt/%d | sh\n", os.Getenv("API_HOST"), os.Getenv("API_PORT"), ID) +
 			"fi\n"
-	} else {
-		//scriptCheck = "PS1 script would go here"
+
+		WinScript = "" +
+			"PS1 script would go here\n"
+	)
+
+	// ID > 1000 represents Unix vulnerabilities scripts.
+	if ID > 1000 {
+		return c.String(http.StatusOK, UnixScript)
 	}
 
-	return c.String(http.StatusOK, scriptCheck)
+	// return Windows script.
+	return c.String(http.StatusOK, WinScript)
+
+}
+
+// GetScript returns a script based on its ID and its package manager.
+func GetScript(c echo.Context) error {
+
+	ID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.String(http.StatusOK, "echo \"[x][VulnaaS] Error: ID is not an integer.\"")
+	}
+
+	scriptConfig, err := scripts.Get(ID)
+	if err != nil {
+		return c.String(http.StatusOK, "echo \"[x][VulnaaS] Error: ID not found.\"")
+	}
+
+	packageManager := c.Param("pm")
+	err = checkPackageManager(packageManager)
+	if err != nil {
+		return c.String(http.StatusOK, "echo \"[x][VulnaaS] Error: Package Manager invalid.\"")
+	}
+
+	switch packageManager {
+	case "yum":
+		return c.String(http.StatusOK, scriptConfig.CmdYum)
+	case "apt":
+		return c.String(http.StatusOK, scriptConfig.CmdApt)
+	case "win":
+		return c.String(http.StatusOK, scriptConfig.CmdWindows)
+	default:
+		return c.String(http.StatusOK, "echo \"[x][VulnaaS] Internal Error: Package Manager.\"")
+	}
+
+}
+
+// checkPackageManager returns an error if a string is equal yum or apt.
+func checkPackageManager(s string) error {
+	if s == "yum" || s == "apt" || s == "win" {
+		return nil
+	}
+	err := errors.New("package manager invalid")
+	return err
 }
