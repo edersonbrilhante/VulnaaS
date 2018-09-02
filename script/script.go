5a19734591a2743c5abea07a7bc64bdf4dad47d2
Rafael dos Santos <rafaveira3@gmail.com>
2018-09-01 23:34:06 -0300

    [FEAT] Add script pckg that holds scripts funcs
    Added GetById that returns an InstallScript based on its ID and
    GetByAlias that returns an InstallScript based on its alias.

---
+++ script/script.go
@@ -0,0 +1,61 @@
+package script
+
+import (
+	"errors"
+
+	"github.com/VulnaaS/VulnaaS-API/config"
+)
+
+// GetByID returns an InstallScript given its ID.
+func GetByID(ID int) (config.InstallScript, error) {
+	var installScript config.InstallScript
+	foundScript := false
+	for _, searchScript := range config.VulnaasConfig.ServiceScripts {
+		if searchScript.ID == ID {
+			foundScript = true
+			installScript = searchScript
+			break
+		}
+	}
+	if !foundScript {
+		for _, searchScript := range config.VulnaasConfig.VulnaasScripts {
+			if searchScript.ID == ID {
+				foundScript = true
+				installScript = searchScript
+				break
+			}
+		}
+	}
+	if !foundScript {
+		errMsg := errors.New("id not found")
+		return installScript, errMsg
+	}
+	return installScript, nil
+}
+
+// GetByAlias returns an InstallScript given its alias.
+func GetByAlias(alias string) (config.InstallScript, error) {
+	var installScript config.InstallScript
+	foundScript := false
+	for _, searchScript := range config.VulnaasConfig.ServiceScripts {
+		if searchScript.Alias == alias {
+			foundScript = true
+			installScript = searchScript
+			break
+		}
+	}
+	if !foundScript {
+		for _, searchScript := range config.VulnaasConfig.VulnaasScripts {
+			if searchScript.Alias == alias {
+				foundScript = true
+				installScript = searchScript
+				break
+			}
+		}
+	}
+	if !foundScript {
+		errMsg := errors.New("alias not found")
+		return installScript, errMsg
+	}
+	return installScript, nil
+}
